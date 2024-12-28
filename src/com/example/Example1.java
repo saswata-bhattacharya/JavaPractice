b487c6b4ed2cffecd178d2884ffc25718366fb7d
Saswata Bhattacharya <saswata.bhattacharya@hotmail.com>
2024-12-28 13:27:22 +0100

    saimpleforloops

--- src/com/example/Example1.java
+++ src/com/example/Example1.java
@@ -1,4 +1,4 @@
-package com.example1;
+package com.example;
 
 public class Example1 {
 
---
+++ src/com/loops/SimpleForLoop.java
@@ -0,0 +1,21 @@
+package com.loops;
+
+public class SimpleForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Simple for loop - Print number from 1 to 5
+//		for (int i = 1; i <= 5; i++)
+//			System.out.println(i);
+
+//		//Reverse Loop
+//		for (int i=5; i>=1; i--)
+//			System.out.println(i);
+
+		for (int i = 5, j = 1; i >= 1; i--, j++)
+			System.out.println(i + " " + j);
+
+	}
+
+}
