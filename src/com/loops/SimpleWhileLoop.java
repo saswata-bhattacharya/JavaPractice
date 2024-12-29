e3b4973426f79ac25331d5db9e8b6e222e6fa289
Saswata Bhattacharya <saswata.bhattacharya@hotmail.com>
2024-12-29 22:55:01 +0100

    While & Do-while loops

---
+++ src/com/loops/SimpleWhileLoop.java
@@ -0,0 +1,15 @@
+package com.loops;
+
+public class SimpleWhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = 1;
+		while (number <= 5) {
+			System.out.println(number);
+			number++;
+		}
+
+	}
+
+}
