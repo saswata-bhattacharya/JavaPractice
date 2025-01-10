6b5a326e5d0bf138ef0ec01fdf12f9a15997690a
Saswata Bhattacharya <saswata.bhattacharya@hotmail.com>
2025-01-10 21:11:44 +0100

    String Methods Part2

---
+++ src/com/strings/StringMethodsPart2.java
@@ -0,0 +1,16 @@
+package com.strings;
+
+public class StringMethodsPart2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String data = "Hello world";
+		
+		
+		String result = data.substring(1,5);
+		System.out.println(result);
+
+	}
+
+}
