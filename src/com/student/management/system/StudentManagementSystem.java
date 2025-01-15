a8f7397bd397ce0008eb9c2273c5573757fabb25
Saswata Bhattacharya <sabha@pandora.net>
2025-01-15 19:14:50 +0100

    StudentManagementSystem - Started

---
+++ src/com/student/management/system/StudentManagementSystem.java
@@ -0,0 +1,20 @@
+package com.student.management.system;
+
+public class StudentManagementSystem {
+
+	public static void main(String[] args) {
+		//Store the information of Student
+		
+		String studentName;
+		int studentAge;
+		double marksObtainedInEnglish;
+		double marksObtainedInScience;
+		double marksObtainedInMaths;
+		String studentGrade;
+		int studentRollNumber;	
+		
+		
+
+	}
+
+}
