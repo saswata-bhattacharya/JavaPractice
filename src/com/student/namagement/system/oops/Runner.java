3a3cfb504aa36745fb31c0fe03605276f0bc82ce
Saswata Bhattacharya <sabha@pandora.net>
2025-01-24 16:02:02 +0100

    this keyword & setter-getter

--- src/com/student/namagement/system/oops/Runner.java
+++ src/com/student/namagement/system/oops/Runner.java
@@ -4,12 +4,25 @@ public class Runner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x = 10;
-		int y[] = new int[3];
-		
 		Student s1 = new Student();
+
+		s1.setName("Debleena");
+		s1.setAge(12);
+		s1.setRollNumber(5);
+		s1.setMarksObtainedInEnglish(77);
+		s1.setMarksObtainedInMaths(85);
+		s1.setMarksObtainedInScience(95);
+		s1.setGrade("A");
 		
-		s1.calculateTotalMarks();
+
+		System.out.println("Student Name: " + s1.getName());
+		System.out.println("Student Age: " + s1.getAge());
+		System.out.println("Student Roll Number: " + s1.getRollNumber());
+		System.out.println("Student Marks in English: " + s1.getMarksObtainedInEnglish());
+		System.out.println("Student Marks in Maths " + s1.getMarksObtainedInMaths());
+		System.out.println("Student Marks in Science: " + s1.getMarksObtainedInScience());
+		System.out.println("Student's Grade: " + s1.getGrade());
+		System.out.println("Total Marks Obtained by Student: "+s1.calculateTotalMarks());
 
 	}
 
--- src/com/student/namagement/system/oops/Student.java
+++ src/com/student/namagement/system/oops/Student.java
@@ -1,18 +1,98 @@
 package com.student.namagement.system.oops;
 
 public class Student {
-	String name;
-	int age;
-	int rollNumber;
-	double marksObtainedInEnglish;
-	double marksObtainedInMaths;
-	double marksObtainedInScience;
-	String grade;
-	
-	public void calculateTotalMarks() {
-		double totalMarks = marksObtainedInEnglish + marksObtainedInMaths + marksObtainedInScience;
-		System.out.println("Total Marks Obtained: "+totalMarks);
+	private String name;
+	private int age;
+	private int rollNumber;
+	private double marksObtainedInEnglish;
+	private double marksObtainedInMaths;
+	private double marksObtainedInScience;
+	private String grade;
+
+	public String getName() {
+		return name;
 	}
-	
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		if (age >= 10 && age <= 18) {
+			this.age = age;
+		} else {
+			System.out.println("Invalid age for High School");
+		}
+	}
+
+	public int getRollNumber() {
+		return rollNumber;
+	}
+
+	public void setRollNumber(int rollNumber) {
+		if (rollNumber >= 0) {
+			this.rollNumber = rollNumber;
+		} else {
+			System.out.println("Invalid roll no");
+		}
+
+	}
+
+	public double getMarksObtainedInEnglish() {
+		return marksObtainedInEnglish;
+	}
+
+	public void setMarksObtainedInEnglish(double marksObtainedInEnglish) {
+		if(marksObtainedInEnglish >= 0 && marksObtainedInEnglish <=100) {
+		this.marksObtainedInEnglish = marksObtainedInEnglish;
+	}
+		else {
+			System.out.println("Invalid Marks");
+		}
+	}
+
+	public double getMarksObtainedInMaths() {
+		return marksObtainedInMaths;
+	}
+
+	public void setMarksObtainedInMaths(double marksObtainedInMaths) {
+
+		if (marksObtainedInMaths >= 0 && marksObtainedInMaths <= 100) {
+			this.marksObtainedInMaths = marksObtainedInMaths;
+		} else {
+			System.out.println("Invalid Marks");
+		}
+
+	}
+
+	public double getMarksObtainedInScience() {
+		return marksObtainedInScience;
+	}
+
+	public void setMarksObtainedInScience(double marksObtainedInScience) {
+		if (marksObtainedInScience >= 0 && marksObtainedInScience <= 100) {
+			this.marksObtainedInScience = marksObtainedInScience;
+		} else {
+			System.out.println("Invalid Marks");
+		}
+
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+	public double calculateTotalMarks() {
+		double totalMarks = marksObtainedInEnglish + marksObtainedInMaths + marksObtainedInScience;
+		return totalMarks;
+	}
 
 }
